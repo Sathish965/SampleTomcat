cad202bc0dff3a0b8ec1ec40b9eb880644127aa0
Sathish965 <cide@santhoshkumar.rk>
2020-09-29 09:02:40 +0000

    first commit

---
+++ src/mypackage/Hello.java
@@ -0,0 +1,16 @@
+package mypackage;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Hello  extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter writer = response.getWriter();
+        writer.write("Hello");
+    }
+}
